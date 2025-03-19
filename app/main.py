b94566dc1932cb1c03411a83ce2edb8523f65ec5
dae22 <dae-22@mail.ru>
2025-03-19 18:00:43 +0300

    Add post route

--- app/main.py
+++ app/main.py
@@ -7,14 +7,21 @@ app = FastAPI()
 DATABASE_URL = "url"
 database = Database(DATABASE_URL)
 
-@app.on_event("startip")
+@app.on_event("startup")
 async def startup():
     await database.connect()
 
-@app.on_event("shotdown")
+@app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
 
-@app.post("/schedule",)
+@app.post("/schedule")
 async def create_schedule(medicine: Medicine):
-    pass
+    query = ("""INSERT INTO schedules (medicine_name, frequency, duration, user_id)
+             VALUES (:medicine_name, :frequency, :duration, :user_id) RETURNING id""")
+    values = medicine.dict()
+    schedule_id = await database.execute(query=query, values=values)
+    return {"schedule_id": schedule_id}
+
+
+
