09f56ba2c47b5552070f81747ddd3527b96a690e
dae22 <dae-22@mail.ru>
2025-03-20 17:27:53 +0300

    Add get routes

--- app/init_db.py
+++ app/init_db.py
@@ -3,17 +3,6 @@ import asyncio
 
 DATABASE_URL = "postgresql://dae22:1998@localhost/mydatabase"
 
-async def create_table_users():
-    conn = await asyncpg.connect(DATABASE_URL)
-    await conn.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        user_id INTEGER UNIQUE,
-        name VARCHAR(100)
-        )
-    ''')
-    await conn.close()
-
 async def create_table_schedules():
     conn = await asyncpg.connect(DATABASE_URL)
     await conn.execute('''
@@ -22,13 +11,12 @@ async def create_table_schedules():
         medicine_name VARCHAR(255) NOT NULL,
         frequency VARCHAR(50) NOT NULL,
         duration INT NOT NULL,
-        start TIMESTAMP WITHOUT TIME ZONE,
+        start TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         end TIMESTAMP WITHOUT TIME ZONE,
-        user_id INT NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users(id) on DELETE CASCADE
+        
+        user_id INT NOT NULL
         )
     ''')
     await conn.close()
 
-asyncio.run(create_table_users())
 asyncio.run(create_table_schedules())
--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from app.models.models import *
+from models.models import *
 from databases import Database
 
 app = FastAPI()
 
 DATABASE_URL = "url"
 database = Database(DATABASE_URL)
+
+def get_daily_schedule():
+    pass
 
 @app.on_event("startup")
 async def startup():
@@ -22,6 +25,28 @@ async def create_schedule(medicine: Medicine):
     values = medicine.dict()
     schedule_id = await database.execute(query=query, values=values)
     return {"schedule_id": schedule_id}
+
+@app.get("/schedules")
+async def get_schedules(user_id: int):
+    query = "SELECT id FROM schedules WHERE user_id=:user_id"
+    values = {"user_id": user_id}
+    rows = await database.fetch_all(query=query, values=values)
+    schedules = [row["id"] for row in rows]
+    return {"schedule": schedules}
+
+@app.get("/schedule")
+async def get_schedule(user_id: int, schedule_id: int):
+    query = "SELECT * FROM schedules WHERE user_id=:user_id AND id=:schedule_id"
+    values = {"user_id": user_id, "schedule_id": schedule_id}
+    row = await database.fetch_one(query=query, values=values)
+    daily_schedule = get_daily_schedule()
+
+@app.get("/next_taking")
+async def take_next_pill(user_id: int)
+    query = "SELECT * FROM scheduled WHERE user_id=:user_id"
+    values = {"user_id": user_id}
+    rows = await database.fetch_all(query=query, values=values)
+
 
 
 
